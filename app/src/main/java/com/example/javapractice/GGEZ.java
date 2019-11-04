0d167020511cfac089d62520c72fb160293b5aa7
Schroga <schroga.official@gmail.com>
2019-11-04 15:16:52 -0600

    something

--- app/src/main/java/com/example/javapractice/GGEZ.java
+++ app/src/main/java/com/example/javapractice/GGEZ.java
@@ -7,10 +7,14 @@ public class GGEZ{
         2.)  Decision Making - greater than less than or equal to 5.
         5.)  Swap Two Numbers - enter #of lines: with a space, create two numbers & print backwards.
         8.)  Binary to Decimal - enter 9 ch max int.(10000000 - 0)
-        11.) Middle of three - enter three numbers with space in between    int num = 2;
+        11.) Middle of three - enter three numbers with space in between
         */
-        System.out.println("Enter an Integer (0-9) to see if it is >, <, or = to 5.");
-        int num = input.nextInt();
-        DecisionMaking.two(num);
+        System.out.println("Enter two numbers with a space in between");
+        String num = input.nextLine();
+        five(num);
+    }
+    public static void five(String num){
+        String num2[] = num.split(" ");
+        System.out.println(num2[1] + " " + num2[0]);
     }
 }
--- app/src/main/java/com/example/javapractice/Schroga.java
+++ app/src/main/java/com/example/javapractice/Schroga.java
@@ -1,9 +1,13 @@
 package com.example.javapractice;
-public class DecisionMaking{
+public class Schroga{
     public static void two(int num){
         if(num < 0 || num > 9)System.exit(420);
         if(num<5)System.out.println(num + " is less than 5.");
         else if(num>5)System.out.println(num + " is more than 5.");
         else System.out.println(num + " is equal to 5.");
+    }
+    public static void five(String num){
+        String num2[] = num.split(" ");
+        System.out.println(num2[1] + " " + num2[0]);
     }
 }
